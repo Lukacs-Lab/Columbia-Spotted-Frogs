fbc1c1a3be181043ded311588f7d7417f89e73f2
williash23 <sarawil@gmail.com>
2015-06-22 15:01:50 -0600

    Updated models and analysis scripts
    For models using sex and interaction covariates

--- Analysis_script.R
+++ Analysis_script.R
@@ -69,10 +69,56 @@
 					nc = 3,
 					debug_mode = F,
 					return_fit = T)		
-					
+		
+		#######  For the following scenarios (with sex covariate), you must use data  ##########
+		#######   subsetted just the individuals for which sex is recorded   ###################
 		#  Scenario 4 - Toe and sex
 		parameters <- c("mean_phi", "p_star", "beta1", "beta2", "pred_surv")
 		fit <- call_jags("toe_n_n_sex_n_n",
+					parallel = F,
+					ni = 5000,
+					nt = 1,
+					nb = 1000,
+					nc = 3,
+					debug_mode = F,
+					return_fit = T)		
+					
+		#  Scenario 5 - Toe, sex and length
+		parameters <- c("mean_phi", "p_star", "beta1", "beta2", "pred_surv")
+		fit <- call_jags("toe_n_length_sex_n_n",
+					parallel = F,
+					ni = 5000,
+					nt = 1,
+					nb = 1000,
+					nc = 3,
+					debug_mode = F,
+					return_fit = T)		
+					
+		#  Scenario 6 - Toe, sex and weight
+		parameters <- c("mean_phi", "p_star", "beta1", "beta2", "pred_surv")
+		fit <- call_jags("toe_weight_n_sex_n_n",
+					parallel = F,
+					ni = 5000,
+					nt = 1,
+					nb = 1000,
+					nc = 3,
+					debug_mode = F,
+					return_fit = T)		
+					
+		#  Scenario 7 - Toe, sex, length and interaction length*sex
+		parameters <- c("mean_phi", "p_star", "beta1", "beta2", "pred_surv")
+		fit <- call_jags("toe_n_length_sex_n_ls",
+					parallel = F,
+					ni = 5000,
+					nt = 1,
+					nb = 1000,
+					nc = 3,
+					debug_mode = F,
+					return_fit = T)		
+					
+		#  Scenario 8 - Toe, sex, weight and interaction weight*sex
+		parameters <- c("mean_phi", "p_star", "beta1", "beta2", "pred_surv")
+		fit <- call_jags("toe_weight_n_sex_ws_n",
 					parallel = F,
 					ni = 5000,
 					nt = 1,
