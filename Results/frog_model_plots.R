6b6be22f6948585235fe4a459700bd1abf6e280f
Sara Williams <sara.williams@cfc.umt.edu>
2016-06-14 14:36:31 -0600

    Dunno

--- Results/frog_model_plots.R
+++ Results/frog_model_plots.R
@@ -13,12 +13,12 @@ x <- read.csv("C:/frog/Frog.csv", as.is = T)
 
 #  Run data extraction function
 source(file.path("C:/Users", 
-				Sys.info()["login"],
-				"Documents/GitHub/Columbia-Spotted-Frogs/data_manip/extract_fun.R"))
+        Sys.info()["login"],
+        "Documents/GitHub/Columbia-Spotted-Frogs/data_manip/extract_fun.R"))
 #  Run data check function
 source(file.path("C:/Users", 
-				Sys.info()["login"],
-				"Documents/GitHub/Columbia-Spotted-Frogs/data_manip/Data_Check_Prep.R"))
+        Sys.info()["login"],
+        "Documents/GitHub/Columbia-Spotted-Frogs/data_manip/Data_Check_Prep.R"))
 
 fEH <- get_data()
 #################################################################################
@@ -35,24 +35,24 @@ frog <- group_by(fEH, Index) %>%
   summarise(len = mean(sc_len)) %>%
   arrange(len) %>%
   mutate(sc_len = as.numeric(scale(len, center = T)),
-					surv = unscale + beta2 * sc_len)
+          surv = unscale + beta2 * sc_len)
 #################################################################################
-	 
+   
 #  Create credible intervals on plots using iterations from MCMC.
 #   Load saved MCMC values as R objects for mean_phi, beta1, and beta2
 load(file.path("C:/Users", 
-										Sys.info()["login"],
-										"Documents/GitHub/Columbia-Spotted-Frogs/Results/sim_reps_mean_phi.RData"))
-				
+                    Sys.info()["login"],
+                    "Documents/GitHub/Columbia-Spotted-Frogs/Results/sim_reps_mean_phi.RData"))
+        
 load(file.path("C:/Users", 
-										Sys.info()["login"],
-										"Documents/GitHub/Columbia-Spotted-Frogs/Results/sim_reps_beta1.RData"))
-				
+                    Sys.info()["login"],
+                    "Documents/GitHub/Columbia-Spotted-Frogs/Results/sim_reps_beta1.RData"))
+        
 load(file.path("C:/Users", 
-										Sys.info()["login"],
-										"Documents/GitHub/Columbia-Spotted-Frogs/Results/sim_reps_beta2.RData"))
+                    Sys.info()["login"],
+                    "Documents/GitHub/Columbia-Spotted-Frogs/Results/sim_reps_beta2.RData"))
 
-#   Unscale mean_phi parameter values for all iterations										
+#   Unscale mean_phi parameter values for all iterations                    
 iter_unscale <- qlogis(sim_reps_mean_phi) # unscaled mean survival over all iteration reps
 
 #  Give a name to the object for the iterations of beta2
@@ -60,11 +60,11 @@ iter_beta2 <- sim_reps_beta2
 
 #   Get value for length, scale and arrange
 iter_len <- group_by(fEH, Index) %>%
-						summarise(len = mean(sc_len)) %>%
-						arrange(len) %>%
-						mutate(sc_len = as.numeric(scale(len, center = T)))
+            summarise(len = mean(sc_len)) %>%
+            arrange(len) %>%
+            mutate(sc_len = as.numeric(scale(len, center = T)))
 #   Separate out just the column "sc_len" and make a numeric vector for use in plot.
-iter_len <- iter_len$sc_len				
+iter_len <- iter_len$sc_len        
 #################################################################################
 
 #  Create plot
@@ -73,22 +73,22 @@ n <- 5000
 keep <- sample(1:length(sim_reps_mean_phi), n, replace = F)
 
 plot(iter_len, seq(0,1,length.out=length(iter_len)), 
-		type="n", 
-		ylim=c(0.05, 0.3),
-		main = "Columbia Spotted Frog Annual Survival",
-		xlab = "Length (scaled and centered)",
-		ylab = "Annual Survival", 
-		bty="L")
+    type="n", 
+    ylim=c(0.05, 0.3),
+    main = "Columbia Spotted Frog Annual Survival",
+    xlab = "Length (scaled and centered)",
+    ylab = "Annual Survival", 
+    bty="L")
 
 for (j in 1:n){
-		iter_surv <- iter_unscale[keep[j]] + iter_beta2[keep[j]] * iter_len
-		lines(iter_len, plogis(iter_surv), col= grey(.9,.3))
-	}
+    iter_surv <- iter_unscale[keep[j]] + iter_beta2[keep[j]] * iter_len
+    lines(iter_len, plogis(iter_surv), col= grey(.9,.3))
+  }
 
-	#  Add line for length and survival, scaled and centered
-	lines (frog$sc_len, plogis(frog$surv), col="grey40", lwd = 2)
-	
-	
+  #  Add line for length and survival, scaled and centered
+  lines (frog$sc_len, plogis(frog$surv), col="grey40", lwd = 2)
+  
+  
  lines (lowess(plogis(frog$surv) ~ frog$sc_len), col="red", lwd = 2)
 
  seq_len <- seq(min(frog$sc_len), max(frog$sc_len), length.out=5000)
