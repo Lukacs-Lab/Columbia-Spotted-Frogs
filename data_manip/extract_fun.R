32235f49c16c7cf41af9c559f4c3d2fc69217d5b
annam21 <anna.moeller@umontana.edu>
2016-09-01 15:45:20 -0600

    Changed gather_ to gather

--- data_manip/extract_fun.R
+++ data_manip/extract_fun.R
@@ -3,21 +3,23 @@
 # Frog Data Extraction
 ################################################################################
 library(stringr)
-library(dplyr)
 library(tidyr)
+library(dplyr)
 
 # Read in data
 x <- read.csv("C:/frog/Frog.csv", as.is = T)
 
 # Make a list where each element is a long data frame of a variable we want
 # col_ext is a pattern to match in original column names
-# col_new is the name of the new column in the long dataset, made from all the wide columns
-# sub_0 is replaces 0s with NA
-# If there are multiple columns that need to be combined into one column, then we do gather
-#     and add prim, sec columns
+# col_new is the name of the new column in the long dataset, made from all 
+#   the wide columns
+# sub_0 is replaces NAs with 0s
+# If there are multiple columns that need to be combined into one column, then we 
+#   do gather and add prim, sec columns
 # If there is only one column to begin with, we just arrange it
 extract_fun <- function(col_ext, col_new, sub_0 = T){
-  #  Takes pattern to match using contains(), must be a character string that matches some column(s) of the data
+  #  Takes pattern to match using contains(), must be a character string that 
+  #    matches some column(s) of the data
   #  Returns a new list containing only the requested data
   if(sub_0){
     x[x == ""] <- 0
@@ -29,7 +31,7 @@ extract_fun <- function(col_ext, col_new, sub_0 = T){
   
   if(ncol(tmp) > 2){
     out <- tmp %>%
-            gather_("occ", col_new, 2:ncol(.), convert = T) %>%
+            gather(occ, col_new, 2:ncol(.), convert = T) %>%
             arrange(Index) %>% 
             mutate(prim = gsub("[A-Za-z]*([1-9][0-9]?)_.*", "\\1", occ),
                    sec = gsub(".*_[A-Za-z]*([1-9]).*", "\\1", occ)) %>%
@@ -43,9 +45,9 @@ extract_fun <- function(col_ext, col_new, sub_0 = T){
 
 # Example call
 xobs <- list("eh" = extract_fun("_Sec", "eh"), 
-              "weight" = extract_fun("Weight", "weight"), 
-              "length" = extract_fun("SVL", "length"),
-              "toes" = extract_fun("toes", "toes"))
+             "weight" = extract_fun("Weight", "weight"), 
+             "length" = extract_fun("SVL", "length"),
+             "toes" = extract_fun("toes", "toes"))
 
 # Things to check: 
 # Is NA different than <NA>?
